bfd4a2817d12331f430932212aad17e13828cc34
Agnes <agnes.rodhe@gmail.com>
2022-12-08 11:35:13 +0100

    updated serversentevents

--- controllers/bike.js
+++ controllers/bike.js
@@ -209,27 +209,25 @@ const deleteOneBike = async (req, res) => {
 
 const getAllActiveBikesEvents = async (req, res) => {
     console.log('request received');
-    if (!req.params.cityId) {
-        res.writeHead(404);
-        console.log("kolla")
-        res.end()
-    } else {
-        res.writeHead(200, {
-            "Connection": "keep-alive",
-            "Content-Type": "text/event-stream",
-            "Cache-Control": "no-cache",
-        });
-        setInterval(async () => {
-            const bikesInCity = await Bike.find({ inCity: req.params.cityId, active: { $ne: null } });
-            res.write('event: ping\n');  // added these
-            res.write(`data: ${JSON.stringify(bikesInCity)}`);
-            // res.write(`data: ${bikesInCity}`);
-            res.write("\n\n");
-            if (bikesInCity.length == 0) {
-                console.log({ error: 'No active bikes in this city' });
-            }
-        }, 5000);
-    }
+    console.log("connected")
+    res.writeHead(200, {
+        "Connection": "keep-alive",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+    });
+    let interval = setInterval(async function() {
+        const bikesInCity = await Bike.find({ inCity: req.params.cityId, active: { $ne: null } });
+        res.write('event: ping\n');  // added these
+        res.write(`data: ${JSON.stringify(bikesInCity)}`);
+        res.write("\n\n");
+        if (bikesInCity.length == 0) {
+            console.log({ error: 'No active bikes in this city' });
+        }
+    }, 2000);
+    req.on("close", () => {
+        clearInterval(interval)
+        console.log("closed");
+    });
 }
 
 module.exports = {
